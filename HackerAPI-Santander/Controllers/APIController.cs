1594e1e3421039b41dabedb1a728896dc2e709c4
Marco Antonio Velloni Figueiredo Junior <marcojunior91@yahoo.com.br>
2019-10-24 15:25:50 +0100

    api controller and model creation

---
+++ HackerAPI-Santander/Controllers/APIController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HackerAPI_Santander.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class APIController : ControllerBase
+    {
+        // GET: api/API
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        [HttpGet]
+        public async Task<JsonResult> ReturnBestStoriesAsync()
+        {
+            //Get first best 20
+            var request = WebRequest.Create(@"https://hacker-news.firebaseio.com/v0/beststories.json");
+            var response = await request.GetResponseAsync().ConfigureAwait(false);
+
+            var responseReader = new StreamReader(response.GetResponseStream());
+            var responseData = await responseReader.ReadToEndAsync();
+
+                
+
+        }
+
+
+        // GET: api/API/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/API
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/API/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ HackerAPI-Santander/Models/Post.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HackerAPI_Santander.Models
+{
+    public class Post
+    {
+        public string title { get; set; }
+        public string uri { get; set; }
+        public string postedBy { get; set; }
+        public DateTime time { get; set; }
+        public int score { get; set; }
+        public int commentCount { get; set; }
+
+
+    }
+}
