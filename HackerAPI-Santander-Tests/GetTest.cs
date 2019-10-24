e4b57bb016cd2605da2dbb56e2ffaa2cee5cf37d
Marco Antonio Velloni Figueiredo Junior <marcojunior91@yahoo.com.br>
2019-10-24 21:25:51 +0100

    Finished UnitTesting

--- HackerAPI-Santander-Tests/GetTest.cs
+++ HackerAPI-Santander-Tests/GetTest.cs
@@ -1,14 +1,28 @@
 using System;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using Xunit;
+using HackerAPI_Santander.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
 
 namespace HackerAPI_Santander_Tests
 {
     public class GetTest
     {
         [Fact]
-        public void TestGet()
+        public async System.Threading.Tasks.Task TestGetAsync()
         {
+            // Arrange
+            var controller = new APIController();
 
+            // Act
+            JObject ret = await controller.ReturnBestStoriesAsync();
+
+            // Assert
+            Assert.NotNull(ret);
+            
         }
     }
 }
--- HackerAPI-Santander/Controllers/APIController.cs
+++ HackerAPI-Santander/Controllers/APIController.cs
@@ -89,30 +89,6 @@ namespace HackerAPI_Santander.Controllers
             return jsonBestPosts;
         }
 
-
-        // GET: api/API/5
-        [HttpGet("{id}", Name = "Get")]
-        public string Get(int id)
-        {
-            return "value";
-        }
-
-        // POST: api/API
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
-        }
-
-        // PUT: api/API/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE: api/ApiWithActions/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
+        
     }
 }
